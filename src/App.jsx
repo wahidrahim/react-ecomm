cfb6e025126df17b5e34068cfa95dd8e4d58dfe6
Wahid Rahim <wahidrahim@gmail.com>
2019-12-22 18:24:46 -0500

    wip - shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => <h1>Hats Page</h1>
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </BrowserRouter>
     </div>
   )
