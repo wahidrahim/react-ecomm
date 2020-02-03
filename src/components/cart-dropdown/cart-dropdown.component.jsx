6334a56ec12c115009b854b76fd8aaaf42eb4cfe
Wahid Rahim <wahidrahim@gmail.com>
2020-02-02 19:57:56 -0500

    toggle cart dropdown when visiting checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,17 +6,23 @@ import { withRouter } from 'react-router'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
 import { getCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartDropdown } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, toggleCartDropdown }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.map(item => (
         <CartItem key={item.id} item={item} />
       ))}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        toggleCartDropdown()
+        history.push('/checkout')
+      }}
+    >
       Check Out
     </CustomButton>
   </div>
@@ -26,4 +32,8 @@ const mapStateToProps = createStructuredSelector({
   cartItems: getCartItems
 })
 
-export default withRouter(connect(mapStateToProps)(CartDropdown))
+const mapDispatchToProps = dispatch => ({
+  toggleCartDropdown: () => dispatch(toggleCartDropdown())
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CartDropdown))
