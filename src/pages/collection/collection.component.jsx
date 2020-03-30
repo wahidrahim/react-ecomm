ff3bdcad1789a5b8a0803fbbfb892b0720f815aa
Wahid Rahim <wahidrahim@gmail.com>
2020-03-30 01:21:50 -0400

    connect collection selector to props

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { selectCollection } from '../../redux/shop/shop.selector'
 
 import './collection.styles.scss'
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = props => (
   <div className="collection-page">
-    <h2>{match.params.collectionId}</h2>
+    {console.log(props)}
+    <h2>{props.match.params.collectionId}</h2>
   </div>
 )
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
