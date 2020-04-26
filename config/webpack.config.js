a6bcc72d906ac04a235eadf97be4c0209106a500
Wahid Rahim <wahidrahim@gmail.com>
2020-04-26 16:46:13 -0400

    add path alias for components

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -301,6 +301,7 @@ module.exports = function(webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+        '@components': path.resolve(__dirname, '../src/components/')
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
