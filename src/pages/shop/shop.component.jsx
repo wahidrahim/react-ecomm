5b15f7fad6271d46d3a0ecfc341ff310face7b1a
Wahid Rahim <wrahim@engagepeople.com>
2020-04-28 16:22:02 -0400

    use sagas to fetch collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
+import {Route} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
 
-import { isCollectionFetching } from '../../redux/shop/shop.selector'
+import {isCollectionFetching} from '../../redux/shop/shop.selector'
 import WithSpinner from '../../components/with-spinner/with-spinner.component'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
-import { fetchCollectionsAsync } from '../../redux/shop/shop.actions'
+import {fetchCollectionsStart} from '../../redux/shop/shop.actions'
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    this.props.fetchCollectionsAsync()
+    this.props.fetchCollectionsStart()
   }
 
   render() {
-    const { match, isCollectionFetching } = this.props
+    const {match, isCollectionFetching} = this.props
 
     return (
       <div className="shop-page">
@@ -52,7 +52,7 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsAsync: () => dispatch(fetchCollectionsAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShopPage)
