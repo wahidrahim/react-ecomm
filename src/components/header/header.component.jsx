8d95e7fa576b00bf957e7aac694a83bb25fa0ac5
Wahid Rahim <wahidrahim@gmail.com>
2020-07-13 04:38:16 -0400

    add sign out saga

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,8 +15,9 @@ import {
   Options,
   OptionLink,
 } from './header.styles';
+import { signOutStart } from '@redux/user/user.actions';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoLink to="/">
       <Logo className="logo" />
@@ -27,7 +28,7 @@ const Header = ({ currentUser, hidden }) => (
         CONTACT
       </OptionLink>
       {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={signOutStart}>
           SIGN OUT
         </OptionLink>
       ) : (
@@ -46,4 +47,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: getCartHidden,
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -8,7 +8,12 @@ import {
 } from '../../firebase/firebase.utils.js';
 
 import UserActionTypes from './user.types';
-import { signInSuccess, signInFailure } from './user.actions';
+import {
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+} from './user.actions';
 
 function* getSnapshotFromUserAuth(userAuth) {
   const userRef = yield call(createUserProfileDocument, userAuth);
@@ -51,6 +56,15 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailure(error));
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -63,10 +77,15 @@ export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(onCheckUserSession),
+    call(onSignOutStart),
   ]);
 }
